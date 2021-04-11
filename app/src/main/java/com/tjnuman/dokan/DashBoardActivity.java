820710938e322b91f05c9175f472f42914fcd31e
TJ-numan <abdullahalnuman235@gmail.com>
2021-04-12 01:25:13 +0600

    DashBoardActivity added

---
+++ app/src/main/java/com/tjnuman/dokan/DashBoardActivity.java
@@ -0,0 +1,23 @@
+package com.tjnuman.dokan;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+    //some code here for the dash board
+
+    }
+}
